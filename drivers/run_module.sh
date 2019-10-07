b0062c76f07638622e65d0695efd7bbf541ffa3f
Arjun Rao <arao@localhost.localdomain>
2019-10-07 17:49:29 -0400

    Updated startup script to account for block device and added some options

--- drivers/run_module.sh
+++ drivers/run_module.sh
@@ -1,22 +1,68 @@
 #!/bin/bash
 
+function printHelp() {
+    echo ""
+    echo "USAGE: $0 -n <name of file> -t <type of device>"
+
+    echo "Options:"
+    echo "  -n|--name   - Name of device (under /dev)"
+    echo "  -t|--type   - Type of device file (b - block, c - character)"
+    echo "  -h|--help   - Print help Message/usage"
+    echo ""
+}
+
 if [[ $EUID -ne 0 ]]; then
     echo "Please run as root!"
     echo "e.g. sudo $0 $@"
     exit -1
 fi
 
-FILE=/dev/simpleBuffer
+while [[ "$1" != "" ]]; do
+    case "$1" in
+        "-h"|"--help")
+            printHelp
+            exit 0
+            ;;
+        "-n"|"--name")
+            shift
+            FILE="$1"
+            shift
+        "-t"|"--type")
+            shift
+            DEVICE_TYPE="$1"
+            shift
+        *)
+            echo "Unrecognized option $1"
+            printHelp
+            exit -1
 
-make simple_buffer.c
+    esac
+done
+
+if [[ -z "$FILE" ]] || [[ -z "$DEVICE_TYPE" ]]; then
+    echo "Missing filename and/or device type!"
+    printHelp
+    exit -1
+fi
+
+#FILE=/dev/simpleBuffer
+
+#make simple_buffer.c
+make
 ret=$?
 
 if [[ $ret -ne 0 ]]; then
     echo "Error compiling module!"
     exit $ret
 fi
+echo "Compile succeeded!"
 
-insmod ./simple_buffer.ko
+#insmod ./simple_buffer.ko
+insmod `basename {$FILE}.ko`
+echo "Installing module..."
+
+sleep 5
+
 ret=$?
 if [[ $ret -ne 0 ]]; then
     echo "Error installing module"
@@ -24,11 +70,12 @@ if [[ $ret -ne 0 ]]; then
 fi
 
 majorNumber=`dmesg | grep Major | tail -1 | awk -F ": " '{print $2}'`
+# if file doesn't exist and we got the majorNumber from dmesg
 if [[ ! -f "$FILE" ]] && [[ ! -z "$majorNumber" ]]; then
-   mknod $FILE c $majorNumber 0 # 0 for minor number
+   mknod $FILE $DEVICE_TYPE $majorNumber 0 # 0 for minor number
 fi
 
-chmod 666 /dev/simpleBuffer
+chmod 666 $FILE
 
 echo "Done! Try writing to file $FILE"
 echo "e.g. echo \"Hello, world!\" > $FILE"

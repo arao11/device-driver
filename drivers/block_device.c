629b3f4c9df8eea2573093e681299a8cf9e43775
Arjun Rao <arao@localhost.localdomain>
2019-10-06 20:51:00 -0400

    Got initial block device compiling, still need to implement queue handler

--- drivers/block_device.c
+++ drivers/block_device.c
@@ -1,93 +1,81 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/genhd.h>
+#include <linux/blkdev.h>
 #include <linux/uaccess.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 /* Function prototypes */
 static int block_init(void);
-static int block_open(struct inode *, struct file *);
-static ssize_t block_write(struct file *, const char *, size_t, loff_t *);
-static ssize_t block_read(struct file *, char *, size_t, loff_t *);
-static int block_release(struct inode *, struct file *);
+static void handle_queue_request(struct request_queue *);
 static void block_exit(void);
 
 /* Constants */
 #define NAME "blockDevice"
+#define PARTITIONS 1
+#define SECTORS 1024 /* Disk size is ~524 KB */
+#define SECTOR_SIZE 512 /* Using standard 512 byte sector size */
 
-static int majorNumber;
+static int majorNumber = 0;
 
-static char kernel_buffer[256] = {0}; // zero out buffer
+/* Our private internal data structure */
+static struct blockDeviceStruct {
+    unsigned long size;
+    struct request_queue *queue;
+    spinlock_t lock;
+    struct gendisk *gd;
+} blockDevice;
 
-static int kernel_buffer_length = 0; // length of whats in buffer
+static struct block_device_operations block_fops = {
+    .owner = THIS_MODULE,
+};
 
-static int numOpen; // number of devices open
-
-
-
-static int buffer_open(struct inode *inode, struct file *filep) {
-    /* Only support for one device open at a time right now */
-    if (numOpen != 0) {
-        printk(KERN_ALERT "Device already open\n");
-        return -EAGAIN;
-    }
-    numOpen++;
-    try_module_get(THIS_MODULE);
-    printk(KERN_INFO "Character device %s opened successfully\n", NAME);
-    return 0;
-}
-
-/**
- * Copy data from userspace into kernel buffer
- * buff: buffer that contains data from userspace. this needs to be copied to the kernel buffer
- * len: length of data in buff
- * offset: 
- */
-static ssize_t buffer_write(struct file *filep, const char *buff, size_t len, loff_t *offset) {
-    printk(KERN_INFO "Writing %s to buffer\n", buff);
-
-    kernel_buffer_length = len;
-    if ((copy_from_user(kernel_buffer, buff, kernel_buffer_length)) < 0) { 
-        printk(KERN_ALERT "Copy buffer to kernel space operation failed\n");
-        return -EFAULT;
-    }
-
-    printk(KERN_INFO "\"%s\" copied successfully to kernel space\n", buff);
-    return kernel_buffer_length;
-}
-
-/**
- * Copy data from buffer in kernel to *buff ptr in userspace
- */
-static ssize_t buffer_read(struct file *filep, char *buff, size_t len, loff_t *offset) {
+static void handle_queue_request(struct request_queue *queue) {
     
-    if (copy_to_user(buff, kernel_buffer, kernel_buffer_length) < 0) {
-        printk(KERN_ALERT "Copy buffer to user space operation failed\n");
-        return -EFAULT;
-    }
-
-    printk(KERN_INFO "\"%s\" copied successfully to user space\n", buff);
-    return len;
 }
-
-/**
- * Release the device so another process can open it
- */
-static int buffer_release(struct inode *inode, struct file *file) {
-    numOpen--;
-    module_put(THIS_MODULE);
-    return 0;
-}
-
 
 static int __init block_init(void) { 
-    /* Passing in 0 dynamically allocates a major number for device */
-    if ((majorNumber = register_blkdev(0, NAME)) < 0) {
+    /* Still use register_blkdev for dynamic majorNumber allocation & getting block device in /proc/devices */
+    if ((majorNumber = register_blkdev(majorNumber, NAME)) <= 0) {
         printk(KERN_ERR "ERROR: Block device %s failed to register (%d)\n", NAME, majorNumber);
         return -majorNumber;
     }
     printk(KERN_INFO "Block device %s registered successfully with Major number: %d\n", NAME, majorNumber);
+
+    blockDevice.gd = alloc_disk(PARTITIONS);
+
+    if (!blockDevice.gd) {
+        printk(KERN_ERR "ERROR: Allocating %s disk\n", NAME);
+    }
+
+    spin_lock_init(&blockDevice.lock);
+
+    /* Setting main fields in gendisk struct */
+    blockDevice.gd->major = majorNumber;
+    blockDevice.gd->first_minor = 0;
+    blockDevice.gd->fops = &block_fops;
+    blockDevice.gd->private_data = &blockDevice;
+
+    /* First we need to set up and allocate queue */
+    blockDevice.queue = blk_init_queue(handle_queue_request, &blockDevice.lock);
+    if (blockDevice.queue == NULL) {
+        printk(KERN_ERR "ERROR: Allocating %s disk block queue\n", NAME);
+    }
+
+    /* Set queue and tell kernel what size sectors we are using for our block */
+    blockDevice.queue->queuedata = &blockDevice;
+    blockDevice.gd->queue = blockDevice.queue;
+    blk_queue_logical_block_size(blockDevice.gd->queue, SECTOR_SIZE); 
+
+    set_capacity(blockDevice.gd, SECTORS);
+
+    /* Name under /proc/partitions and /sys/block */
+    snprintf(blockDevice.gd->disk_name, 32, NAME); 
+
+    /* Add disk last since this activates disk for usage */
+    add_disk(blockDevice.gd);
     return 0;
 }
 
@@ -95,10 +83,15 @@ static int __init block_init(void) {
  * Cleanup module, and unregister char device
  */
 static void __exit block_exit(void) {
-    printk(KERN_ALERT "Exiting block device\n");
+    printk(KERN_ALERT "Exiting block device %s\n", NAME);
+
+    /* Need to delete queue */
+    if (blockDevice.gd->queue) {
+        blk_cleanup_queue(blockDevice.queue);
+    }
     unregister_blkdev(majorNumber, NAME);
 }
 
 
-module_init(buffer_init);
-module_exit(buffer_exit);
+module_init(block_init);
+module_exit(block_exit);

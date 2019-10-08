a9e6611c6cde3659e970201c8bbf91c5f6ce6ee8
TahaBilalCS <theswag@vt.edu>
2019-10-08 18:54:26 -0400

    Updates

--- syscall/hello/hello.c
+++ syscall/hello/hello.c
@@ -12,15 +12,33 @@ SYSCALL_DEFINE3(hello, int, key, char *, data, int, action) {
 //Action is an int, we can do 0 for add and 1 for delete/vice versa
     printk("sys_hello : %d, %s, %d\n",key, data, action);
 	
-    DECLARE_HASHTABLE(data_hash, 32);
-    hash_init(data_hash);
+    
 
-    struct h_struct {
+struct h_struct {
 	int key1;
 	char *entry;
 	struct hlist_node node;
 };
 
+struct h_struct first = {
+	.key1 = key;
+	.entry = data;
+	.node = 0;
+};
+
+
+DECLARE_HASHTABLE(a, 3);
+hash_init(a);
+
+hash_add(a, &first.next, first.data);
+
+int bkt;
+struct h_struct * current;
+hash_for_each_entry(a, bkt, current, next){
+	printk(KERN_INFO "data=%d is in bucket %d\n", current->entry, bkt);
+}
+
+/*
 struct hlist_head {
     struct hlist_node *first;
 };
@@ -28,6 +46,7 @@ struct hlist_head {
 struct hlist_node {
     struct hlist_node *next, **pprev;
 };
+*/
 
 //hash_add(data_hash, "entry node", key);
 
